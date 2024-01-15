8f84c334bc7c54413490a90a7787d39c827187ac
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-01-15 10:28:12 -0300

    atividade2

--- angular-atividade1/src/app/app.routes.ts
+++ angular-atividade1/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { AboutpageComponent } from './pages/aboutpage/aboutpage.component';
+import { ContactpageComponent } from './pages/contactpage/contactpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HomepageComponent },
+    { path: "sobre", component: AboutpageComponent },
+    { path: "contato", component: ContactpageComponent }
+];
