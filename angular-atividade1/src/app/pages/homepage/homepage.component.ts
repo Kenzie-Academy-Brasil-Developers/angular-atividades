d4728d63d23428d5149c7896c02dc865af603ecb
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-01-31 09:31:55 -0300

    atividade4

--- angular-atividade1/src/app/pages/homepage/homepage.component.ts
+++ angular-atividade1/src/app/pages/homepage/homepage.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductCardComponent],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.css'
+  styleUrl: './homepage.component.css',
 })
 export class HomepageComponent {
-
+  productList = [
+    {
+      name: 'Computador',
+      price: 3000,
+    },
+    {
+      name: 'Notebook',
+      price: 4500,
+    },
+    {
+      name: 'Mouse',
+      price: 200,
+    },
+  ];
 }
